62f6f98016e145bcb4d51b479679b1f9395d494b
PetrenkoA <PetrenkoA@users.noreply.github.com>
2018-09-21 15:04:15 +0300

    Add files via upload

---
+++ OpenSoftTest/ExcelScanner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Windows.Media;
+
+namespace OpenSoftTest
+{
+    class ExcelScanner: IDisposable
+    {
+        Application excelApplication;
+
+        public ExcelScanner()
+        {
+            excelApplication = new Application();
+        }
+
+        public void paintWords(string filePath, string word, XlRgbColor color)
+        {
+            Workbook workBook = excelApplication.Workbooks.Open(filePath);
+            int sheets = workBook.Sheets.Count;
+
+            try
+            {
+                for (int i = 1; i < sheets + 1; i++)
+                {
+                    Worksheet sheet = (Worksheet)excelApplication.Sheets[i];
+                    if (sheet.UsedRange.Count > 1)
+                    {
+                        object[,] valueArray = (object[,])sheet.UsedRange.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+                        for (int h = 1; h < valueArray.GetLength(0) + 1; h++)
+                            for (int m = 1; m < valueArray.GetLength(1) + 1; m++)
+                                if (valueArray[h, m] != null && valueArray[h, m].ToString() == word) sheet.Cells[h, m].Interior.Color = color;
+
+                    }
+                    else
+                    {
+                        string value = sheet.UsedRange.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+                        if (value == word) sheet.Cells[sheet.UsedRange.Row, sheet.UsedRange.Column].Interior.Color = XlRgbColor.rgbRed;
+                    }
+                }
+                workBook.SaveAs(filePath, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+            }
+            finally
+            {
+                workBook.Close(false, filePath, null);
+                Marshal.ReleaseComObject(workBook);
+            }
+        }
+
+        public void Dispose()
+        {
+            Marshal.ReleaseComObject(excelApplication);
+        }
+
+
+    }
+}
---
+++ OpenSoftTest/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+
+namespace OpenSoftTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_browse_Click(object sender, RoutedEventArgs e)
+        {
+            using(OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Excel Files (*.xlsx;*.xlsm;*.xls;*.xlsb)|*.xlsx;*.xlsm;*.xls;*.xlsb";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) tbx_path.Text = dialog.FileName;
+            }
+        }
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew((path) => performExcelTask(path.ToString()), tbx_path.Text);
+        }
+
+        public void performExcelTask(string path)
+        {
+            using (ExcelScanner scanner = new ExcelScanner())
+            {
+                scanner.paintWords(path, "ReD", Microsoft.Office.Interop.Excel.XlRgbColor.rgbRed);
+            }
+        }
+
+    }
+}
